2b1226c2c8309e31991cc495f7e977e34bf4b5ff
codewangz <309742371@163.com>
2021-11-19 14:57:51 +0800

    service
    service

---
+++ service/service.go
@@ -0,0 +1,241 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type base struct {
+}
+
+var serviceMap map[string]interface{}
+
+type Result interface{}
+
+type Params map[string]interface{}
+
+func (p Params) GetInt(key string, defVal ...int64) int64 {
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case float64:
+		return int64(val.(float64))
+	case int64:
+		return val.(int64)
+	case int:
+		return int64(val.(int))
+	case string:
+		if v, err := strconv.ParseInt(val.(string), 10, 64); err == nil {
+			return v
+		}
+	}
+
+	return 0
+}
+
+func (p Params) GetFloat(key string, defVal ...float64) float64 {
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case float64:
+		return val.(float64)
+	case string:
+		if v, err := strconv.ParseFloat(val.(string), 64); err == nil {
+			return v
+		}
+	}
+
+	return 0
+}
+
+func (p Params) GetString(key string, defVal ...string) string {
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case float64:
+		return strconv.FormatFloat(val.(float64), 'f', -1, 64)
+	case string:
+		return val.(string)
+	}
+
+	return ""
+}
+
+func (p Params) GetMap(key string, defVal ...map[string]interface{}) map[string]interface{} {
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case map[string]interface{}:
+		return val.(map[string]interface{})
+	}
+	return nil
+}
+
+func (p Params) GetSlice(key string, defVal ...[]interface{}) []interface{} {
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case []interface{}:
+		return val.([]interface{})
+	}
+	return nil
+}
+
+func (p Params) GetIntSlice(key string, defVal ...[]int64) []int64 {
+	result := []int64{}
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case []interface{}:
+		for _, v := range val.([]interface{}) {
+			switch v.(type) {
+			case float64:
+				result = append(result, int64(v.(float64)))
+			case string:
+				if v, err := strconv.ParseInt(v.(string), 10, 64); err == nil {
+					result = append(result, v)
+				}
+			}
+		}
+	}
+	return result
+}
+
+func (p Params) GetStringSlice(key string, defVal ...[]string) []string {
+	result := []string{}
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case []interface{}:
+		for _, v := range val.([]interface{}) {
+			switch v.(type) {
+			case float64:
+				result = append(result, strconv.FormatFloat(v.(float64), 'f', -1, 64))
+			case int:
+				result = append(result, strconv.Itoa(v.(int)))
+			case int64:
+				result = append(result, strconv.FormatInt(v.(int64), 10))
+			case string:
+				result = append(result, v.(string))
+			}
+		}
+	case []string:
+		for _, v := range val.([]string) {
+			result = append(result, v)
+		}
+	case []int:
+		for _, v := range val.([]int) {
+			result = append(result, strconv.Itoa(v))
+		}
+	case []int64:
+		for _, v := range val.([]int64) {
+			result = append(result, strconv.FormatInt(v, 10))
+		}
+	}
+	return result
+}
+
+func (p Params) GetStringSliceSlice(key string, defVal ...[][]string) [][]string {
+	result := [][]string{}
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	switch val.(type) {
+	case []interface{}:
+		for _, v := range val.([]interface{}) {
+			tempV := []string{}
+			switch v.(type) {
+			case []interface{}:
+				for _, va := range v.([]interface{}) {
+					switch va.(type) {
+					case float64:
+						tempV = append(tempV, strconv.FormatFloat(va.(float64), 'f', -1, 64))
+					case string:
+						tempV = append(tempV, va.(string))
+					}
+				}
+			}
+			result = append(result, tempV)
+		}
+	}
+	return result
+}
+
+func (p Params) GetVal(key string, defVal ...interface{}) interface{} {
+	val := p[key]
+	if val == nil && len(defVal) > 0 {
+		return defVal[0]
+	}
+	return val
+}
+
+func Call(service string, params Params) Result {
+	arr := strings.Split(service, ".")
+	if len(arr) != 2 {
+		panic(fmt.Errorf("params error"))
+	}
+	name := arr[0]
+	if name == "" {
+		panic(fmt.Errorf("service name error"))
+	}
+	method := arr[1]
+	if method == "" {
+		panic(fmt.Errorf("service method error"))
+	}
+	obj := serviceMap[name+"Service"]
+	if obj == nil {
+		panic(fmt.Errorf("%sService is not exist", name))
+	}
+	Method := reflect.ValueOf(obj).MethodByName(method)
+	if Method.Kind() == reflect.Invalid {
+		panic(fmt.Errorf("service method %s is not exist", method))
+	}
+	args := make([]reflect.Value, Method.Type().NumIn())
+	if len(args) > 0 {
+		args[0] = reflect.ValueOf(params)
+	}
+	result := Method.Call(args)
+	if len(result) > 1 && result[1].Interface() != nil {
+		panic(result[1].Interface().(error))
+	}
+	if (len)(result) > 0 {
+		return result[0].Interface()
+		/*		if result[0].Interface() != nil {
+				return result[0].Interface().(Result)
+			}*/
+	}
+	return nil
+}
+
+func RegisterService(services ...interface{}) {
+	if serviceMap == nil {
+		serviceMap = map[string]interface{}{}
+	}
+	for _, service := range services {
+		value := reflect.ValueOf(service)
+		value = reflect.Indirect(value)
+		name := value.Type().Name()
+		serviceMap[name] = service
+	}
+}
+
+func init() {
+	serviceMap = make(map[string]interface{})
+}
---
+++ utils/function.go
@@ -0,0 +1,350 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func Ucfirst(str string) string {
+	if len(str) < 1 {
+		return ""
+	}
+	stdArr := []rune(str)
+	if stdArr[0] >= 97 && stdArr[0] <= 122 {
+		stdArr[0] -= 32
+	}
+	return string(stdArr)
+}
+
+func Lcfirst(str string) string {
+	if len(str) < 1 {
+		return ""
+	}
+	stdArr := []rune(str)
+	if stdArr[0] >= 65 && stdArr[0] <= 90 {
+		stdArr[0] += 32
+	}
+	return string(stdArr)
+}
+
+func ItoString(val interface{}) string {
+	str := ""
+	switch val.(type) {
+	case float64:
+		str = strconv.FormatFloat(val.(float64), 'f', -1, 64)
+	case string:
+		str = val.(string)
+	case int64:
+		str = strconv.FormatInt(val.(int64), 10)
+	case int:
+		str = strconv.Itoa(val.(int))
+	case error:
+		str = val.(error).Error()
+	}
+	return str
+}
+
+func ToSliceString(val interface{}) []string {
+	result := []string{}
+	switch x := val.(type) {
+	case []interface{}:
+		for _, v := range x {
+			result = append(result, ItoString(v))
+		}
+	case []string:
+		for _, v := range x {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func ToSliceInt64(val interface{}) []int64 {
+	var result []int64
+	switch x := val.(type) {
+	case []interface{}:
+		for _, v := range x {
+			result = append(result, ToInt64(v))
+		}
+	}
+	return result
+}
+
+func ToMapInterface(val interface{}) map[string]interface{} {
+	switch val.(type) {
+	case map[string]interface{}:
+		return val.(map[string]interface{})
+	}
+	return nil
+}
+
+func RemoveRepeatedElement(arr []string) (newArr []string) {
+	newArr = make([]string, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return newArr
+}
+
+func CheckPhone(tel string) bool {
+
+	reg := `^1([38][0-9]|14[579]|5[^4]|16[6]|7[1-35-8]|9[189])\d{8}$`
+
+	rgx := regexp.MustCompile(reg)
+
+	return rgx.MatchString(tel)
+}
+
+func SortMap(data map[string]interface{}) []interface{} {
+	var keys []string
+	for key, _ := range data {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	var sliceData []interface{}
+	for _, key := range keys {
+		sliceData = append(sliceData, data[key])
+	}
+	return sliceData
+}
+
+func InSlice(items []string, item string) bool {
+	for _, eachItem := range items {
+		if eachItem == item {
+			return true
+		}
+	}
+	return false
+}
+
+func ToInt(val interface{}) int {
+	num := 0
+	switch val.(type) {
+	case float64:
+		num = int(val.(float64))
+	case string:
+		num, _ = strconv.Atoi(val.(string))
+	case int64:
+		num = int(val.(int64))
+	case int:
+		num = val.(int)
+	}
+	return num
+}
+
+func ToInt64(val interface{}) int64 {
+	var num int64
+	switch val.(type) {
+	case float64:
+		num = int64(val.(float64))
+	case string:
+		num, _ = strconv.ParseInt(val.(string), 10, 64)
+	case int64:
+		num = val.(int64)
+	case int:
+		num = int64(val.(int))
+	}
+	return num
+}
+
+func DeleteFromSlice(element interface{}, slice interface{}) {
+	switch slice := (slice).(type) {
+	case *[]string:
+		for key, val := range *slice {
+			if val == element {
+				*slice = append((*slice)[:key], (*slice)[key+1:]...)
+			}
+		}
+	}
+}
+
+func SliceInt64Sum(slice []int64) (result int64) {
+	for _, val := range slice {
+		result += val
+	}
+	return
+}
+
+func ToSliceInterface(val interface{}) (result []interface{}) {
+	switch val.(type) {
+	case []interface{}:
+		return val.([]interface{})
+	}
+	return
+}
+
+func MapMerge(map1, map2 map[string]interface{}) (result map[string]interface{}) {
+	result = Copy(map1).(map[string]interface{})
+	for key, val := range map2 {
+		result[key] = val
+	}
+	return
+}
+
+func ToSnakes(data []map[string]interface{}) []map[string]interface{} {
+	var result []map[string]interface{}
+	for _, val := range data {
+		temp := map[string]interface{}{}
+		for key, value := range val {
+			temp[snakeString(key)] = value
+		}
+		result = append(result, temp)
+	}
+	return result
+}
+
+func ToSnake(data map[string]interface{}) map[string]interface{} {
+	result := map[string]interface{}{}
+	for key, value := range data {
+		result[snakeString(key)] = value
+	}
+	return result
+}
+
+/**
+ * 驼峰转蛇形 snake string
+ * @description XxYy to xx_yy , XxYY to xx_y_y
+ * @date 2020/7/30
+ * @param s 需要转换的字符串
+ * @return string
+ **/
+func snakeString(s string) string {
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	num := len(s)
+	for i := 0; i < num; i++ {
+		d := s[i]
+		// or通过ASCII码进行大小写的转化
+		// 65-90（A-Z），97-122（a-z）
+		//判断如果字母为大写的A-Z就在前面拼接一个_
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, '_')
+		}
+		if d != '_' {
+			j = true
+		}
+		data = append(data, d)
+	}
+	//ToLower把大写字母统一转小写
+	return strings.ToLower(string(data[:]))
+}
+
+/**
+ * 蛇形转驼峰
+ * @description xx_yy to XxYx  xx_y_y to XxYY
+ * @date 2020/7/30
+ * @param s要转换的字符串
+ * @return string
+ **/
+func camelString(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && d == '_' && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}
+
+func JsonDecode(jsonStr string) (result interface{}) {
+	if jsonStr == "" {
+		return
+	}
+	if err := json.Unmarshal([]byte(jsonStr), &result); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func JsonEncode(obj interface{}) string {
+	if obj == nil {
+		return ""
+	}
+	result, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+	return string(result)
+}
+
+func Now() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func Copy(data interface{}) interface{} {
+	var result interface{}
+	jsonByte, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(jsonByte, &result)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func MD5(str string) string {
+	md5 := md5.New()
+	md5.Write([]byte(str))
+	return hex.EncodeToString(md5.Sum(nil))
+}
+
+func ToFloat64(val interface{}) (result float64) {
+	switch item := val.(type) {
+	case string:
+		result, _ = strconv.ParseFloat(item, 64)
+		return result
+	case int64:
+		return float64(item)
+	case int:
+		return float64(item)
+	case float64:
+		return item
+	case float32:
+		return float64(item)
+	case nil:
+		return 0
+	}
+	return 0
+}
+
+func MapJoin(m map[string]interface{}, split string) (str string) {
+	for _, v := range m {
+		if str != "" {
+			str += split + ItoString(v)
+		} else {
+			str += ItoString(v)
+		}
+	}
+	return
+}
